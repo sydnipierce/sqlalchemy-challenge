1f4235d59057b41bcd42b0032617d092938507b8
Sydni <sydnimpierce@gmail.com>
2021-06-23 23:59:50 -0400

    Completed temps page

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
+import datetime as dt
 
 from flask import Flask, jsonify
 
@@ -54,8 +55,7 @@ def precip():
 
     for row in raw_data:
         result_dict = {}
-        result_dict["date"] = row.date
-        result_dict["prcp"] = row.prcp
+        result_dict[row.date] = row.prcp
         result_list.append(result_dict)
 
     return(jsonify(result_list))
@@ -71,6 +71,36 @@ def stations():
         result_dict["id"] = row.id
         result_dict["station"] = row.station
         result_dict["name"] = row.name
+        result_list.append(result_dict)
+
+    return(jsonify(result_list))
+
+@app.route("/api/v1.0/tobs")
+def temps():
+    dates_ordered = session.query(Measurement.date).\
+        order_by(Measurement.date.desc()).first()
+
+    last_date = str(dates_ordered[0])
+    last_date_dt = dt.datetime.strptime(last_date, "%Y-%m-%d")
+    year_ago = last_date_dt - dt.timedelta(days=365)
+    
+    station_use = session.query(Measurement.station, func.count(Measurement.station)).\
+        group_by(Measurement.station).\
+        order_by(func.count(Measurement.station).desc()).all()
+
+    most_active = station_use[0][0]
+
+    data_last_year = session.query(Measurement.date, Measurement.tobs).\
+        filter(Measurement.station == most_active).\
+        filter(Measurement.date > year_ago).\
+        filter(Measurement.date <= last_date_dt).all()
+
+    result_list = []
+
+    for row in data_last_year:
+        result_dict = {}
+        result_dict["date"] = row.date
+        result_dict["tobs"] = row.tobs
         result_list.append(result_dict)
 
     return(jsonify(result_list))
